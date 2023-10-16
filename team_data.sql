96d40aa2b91d8fac69a842018860f958d4b6b246
Charles Watson <charlie.g.watson@gmail.com>
2023-10-16 14:20:57 +0100

    Created new doc with the team information

---
+++ team_data.sql
@@ -0,0 +1,8 @@
+use superheroe_activity;
+
+insert into team(team_name,objective)
+values('JLA','Protect the world'),
+('JSA','Defeat the Nazis'),
+('Birds of Prey','Fight crime (without men)'),
+('Task Force X',"Follow Waller's orders or die"),
+('Teem Titans','Teach young superheros to be their best');
