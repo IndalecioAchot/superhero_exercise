12592aabba42fb9706af54d81d14c2b9e0d27664
Charles Watson <charlie.g.watson@gmail.com>
2023-10-16 14:27:36 +0100

    Added code to fill hero table

--- team_data.sql
+++ team_data.sql
@@ -6,3 +6,15 @@ values('JLA','Protect the world'),
 ('Birds of Prey','Fight crime (without men)'),
 ('Task Force X',"Follow Waller's orders or die"),
 ('Teem Titans','Teach young superheros to be their best');
+
+insert into hero(hero_name,alias,team_id,ability_id)
+values('Bruce Wayne','Batman',1,1),
+('Clark Kent','Superman',1,2),
+('Jay Garrick','The Flash',2,3),
+('Alan Scott','Green Lantern',2,4),
+('Helena Bertenelli','The Huntress',3,5),
+('Dr. Harleen Quinzel','Harley Quinn',3,6),
+('Floyd Lawton','Deadshot',4,7),
+('Cecil Adams','Count Vertigo',4,8),
+('Damian Wayne','Robin',5,9),
+('Dick Grayson','Nightwing',5,10);
